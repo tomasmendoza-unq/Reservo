c1452dd4bc5e89a8ad8b0dee830606ebd02b0e1e
Tomas Mendoza <tm1453766@gmail.com>
2026-04-22 00:09:35 -0300

    refactor: remuevo carpetas de node modules y extraigo la config de cors al archivo web config en el server

--- server/src/main/java/com/reservo/controller/ReservaControllerREST.java
+++ server/src/main/java/com/reservo/controller/ReservaControllerREST.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/mis-reservas")
 public final class ReservaControllerREST {
     private final PeticionService peticionService;
